766fb5eebd5e6b2211a30afb78b3bf8ba03c0638
Rejtő István <reistvan@gmail.com>
2019-06-14 19:28:06 +0200

    Refactor code to handle operations on the same way
    This means some general solution to the '+' and '-'
    operations to make easy to add new one.

--- src/main/java/eistrej/calculator/evaluator/Evaluator.java
+++ src/main/java/eistrej/calculator/evaluator/Evaluator.java
@@ -1,18 +1,31 @@
 package eistrej.calculator.evaluator;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.IntBinaryOperator;
+
 public class Evaluator {
     public int evaluate(String input) {
         if (input.matches("\\s*\\d+\\s*")) {
             return Integer.parseInt(input.trim());
         }
-        String[] tokens = input.split("\\+");
-        if (tokens.length > 1) {
-            return evaluate(tokens[0]) + evaluate(tokens[1]);
+        Map<String, IntBinaryOperator> operations = new LinkedHashMap<>();
+        operations.put("\\+", (left, right) -> left + right);
+        operations.put("-", (left, right) -> left - right);
+        String[] tokens = null;
+        IntBinaryOperator operator = null;
+        Iterator<String>  iterator = operations.keySet().iterator();
+        while (iterator.hasNext()) {
+            String regepx = iterator.next();
+            tokens = input.split(regepx);
+            if (tokens.length > 1) {
+                operator = operations.get(regepx);
+                break;
+            }
         }
-        tokens = input.split("-");
-        if (tokens.length > 1) {
-            return evaluate(tokens[0]) - evaluate(tokens[1]);
-        }
-        return 4;
+//        String left = String.join("+", Arrays.copyOfRange(tokens, 1, tokens.length));
+        return operator.applyAsInt(evaluate(tokens[0]), evaluate(tokens[1]));
     }
 }
