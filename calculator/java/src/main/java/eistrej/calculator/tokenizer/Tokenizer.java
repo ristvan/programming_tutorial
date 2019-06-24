be6d774b91a61cfae68ff83925d357d6b0ae2e36
eistrej <reistvan@gmail.com>
2019-06-24 10:18:46 +0200

    Move generator initializations into constructor of Tokenizer
    Some coding style change in files.

--- calculator/java/src/main/java/eistrej/calculator/tokenizer/Tokenizer.java
+++ calculator/java/src/main/java/eistrej/calculator/tokenizer/Tokenizer.java
@@ -9,20 +9,20 @@ import java.util.List;
 
 public class Tokenizer implements ITokenizer {
     private String expression;
+    private final List<ITokenCreator> tokenCreators;
 
     public Tokenizer(String expression) {
         this.expression = expression.trim();
-    }
-
-    @Override
-    public IToken getNextToken() {
-        List<ITokenCreator> tokenCreators = new LinkedList<>();
+        tokenCreators = new LinkedList<>();
         tokenCreators.add(new NumberTokenCreator());
         tokenCreators.add(new AdditionTokenGenerator());
         tokenCreators.add(new MinusTokenGenerator());
         tokenCreators.add(new MultiplicationTokenGenerator());
         tokenCreators.add(new DivisionTokenGenerator());
+    }
 
+    @Override
+    public IToken getNextToken() {
         IToken token = null;
         for (ITokenCreator tokenCreator : tokenCreators) {
             if (tokenCreator.isMatching(expression)) {
--- calculator/java/src/main/java/eistrej/calculator/tokenizer/tokens/generators/NumberTokenCreator.java
+++ calculator/java/src/main/java/eistrej/calculator/tokenizer/tokens/generators/NumberTokenCreator.java
@@ -4,21 +4,22 @@ import eistrej.calculator.tokenizer.tokens.INumber;
 import eistrej.calculator.tokenizer.tokens.IToken;
 
 public class NumberTokenCreator implements ITokenCreator {
-    private String expression=null;
-    private int index=0;
+    private String expression = null;
+    private int index = 0;
 
     @Override
     public boolean isMatching(String expression){
-        this.expression=expression;
+        this.expression = expression;
+        index = 0;
         return expression.length() > 0 && Character.isDigit(expression.charAt(0));
     }
 
     @Override
     public IToken getToken(){
-        while(index<expression.length()&&Character.isDigit(expression.charAt(index))){
+        while (index<expression.length() && Character.isDigit(expression.charAt(index))){
             index++;
         }
-        String numberAsString=expression.substring(0,index);
+        String numberAsString = expression.substring(0, index);
         return (INumber) () -> Integer.parseInt(numberAsString);
     }
 
