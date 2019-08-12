4b390a4f03b7b8d764d4da485cd1f471187e6796
Rejtő István <reistvan@gmail.com>
2019-08-12 11:54:10 +0200

    Add functionality any number of same operations
    This change is lead us to be able to handle multiple:
     * additions (+)
     * subtractions (-)
     * multiplcations (*)
     * divisions (/)
    
    This is valid only when only the same type is used within expression.

--- calculator/java/src/main/java/eistrej/calculator/semantic/ExpressionCreator.java
+++ calculator/java/src/main/java/eistrej/calculator/semantic/ExpressionCreator.java
@@ -6,6 +6,12 @@ import eistrej.calculator.tokenizer.tokens.*;
 
 import java.util.Stack;
 
+interface IExpressionWrapper {
+    void setLeft(IExpression expression);
+    void setRight(IExpression expression);
+    IExpression getExpression();
+}
+
 public class ExpressionCreator {
     private final ITokenizer tokenizer;
 
@@ -14,12 +20,37 @@ public class ExpressionCreator {
     }
 
     public IExpression createExpression() {
-        INumber left = (INumber) tokenizer.getNextToken();
-        IToken operation = tokenizer.getNextToken();
-        INumber right = (INumber) tokenizer.getNextToken();
-        if (operation == null) {
-            return new NumberExpression(left.getValue());
+        Stack<IExpression> expressions = new Stack<>();
+        Stack<IExpressionWrapper> operators = new Stack<>();
+        IToken token = tokenizer.getNextToken();
+        while (token != null) {
+            if (token instanceof INumber) {
+                INumber number = (INumber) token;
+                expressions.push(new NumberExpression(number.getValue()));
+            } else if (operators.empty()) {
+                operators.push(createOperatorWrapper(token));
+            } else {
+                IExpression right = expressions.pop();
+                IExpression left = expressions.pop();
+                IExpressionWrapper expressionWrapper = createOperatorWrapper(token);
+                expressionWrapper.setLeft(left);
+                expressionWrapper.setRight(right);
+                expressions.push(expressionWrapper.getExpression());
+            }
+            token = tokenizer.getNextToken();
         }
+        while (!operators.empty()) {
+            IExpression right = expressions.pop();
+            IExpression left = expressions.pop();
+            IExpressionWrapper expressionWrapper = operators.pop();
+            expressionWrapper.setLeft(left);
+            expressionWrapper.setRight(right);
+            expressions.push(expressionWrapper.getExpression());
+        }
+        return expressions.pop();
+    }
+
+    private IExpression getExpression(INumber left, IToken operation, INumber right) {
         if (operation instanceof IAddition) {
             AdditionExpression expression = new AdditionExpression();
             expression.setAugend(new NumberExpression(left.getValue()));
@@ -45,5 +76,83 @@ public class ExpressionCreator {
             return expression;
         }
         return null;
+    }
+
+    private IExpressionWrapper createOperatorWrapper(IToken operation) {
+        IExpressionWrapper operatorWrapper = null;
+        if (operation instanceof IAddition) {
+            operatorWrapper = new IExpressionWrapper() {
+                AdditionExpression addition = new AdditionExpression();
+                @Override
+                public void setLeft(IExpression expression) {
+                    addition.setAugend(expression);
+                }
+
+                @Override
+                public void setRight(IExpression expression) {
+                    addition.setAddend(expression);
+                }
+
+                @Override
+                public IExpression getExpression() {
+                    return addition;
+                }
+            };
+        } else if (operation instanceof IMinus) {
+            operatorWrapper = new IExpressionWrapper() {
+                SubtractionExpression subtraction = new SubtractionExpression();
+                @Override
+                public void setLeft(IExpression expression) {
+                    subtraction.setMinuend(expression);
+                }
+
+                @Override
+                public void setRight(IExpression expression) {
+                    subtraction.setSubtrahend(expression);
+                }
+
+                @Override
+                public IExpression getExpression() {
+                    return subtraction;
+                }
+            };
+        } else if (operation instanceof IMultiplication) {
+            operatorWrapper = new IExpressionWrapper() {
+                MultiplicationExpression multiplication = new MultiplicationExpression();
+                @Override
+                public void setLeft(IExpression expression) {
+                    multiplication.setMultiplier(expression);
+                }
+
+                @Override
+                public void setRight(IExpression expression) {
+                    multiplication.setMulticand(expression);
+                }
+
+                @Override
+                public IExpression getExpression() {
+                    return multiplication;
+                }
+            };
+        } else if (operation instanceof IDivision) {
+            operatorWrapper = new IExpressionWrapper() {
+                DivisionExpression division = new DivisionExpression();
+                @Override
+                public void setLeft(IExpression expression) {
+                    division.setDividend(expression);
+                }
+
+                @Override
+                public void setRight(IExpression expression) {
+                    division.setDivisor(expression);
+                }
+
+                @Override
+                public IExpression getExpression() {
+                    return division;
+                }
+            };
+        }
+        return operatorWrapper;
     }
 }
--- calculator/java/src/test/java/eistrej/calculator/semantic/ExpressionCreatorTest.java
+++ calculator/java/src/test/java/eistrej/calculator/semantic/ExpressionCreatorTest.java
@@ -97,10 +97,10 @@ public class ExpressionCreatorTest {
         ExpressionCreator ec = new ExpressionCreator(tokenizer);
         IExpression expression = ec.createExpression();
         assertTrue("Expression is NOT an Subtraction", expression instanceof SubtractionExpression);
-        SubtractionExpression addExpression = (SubtractionExpression) expression;
-        NumberExpression leftNumber = (NumberExpression) addExpression.getMinuend();
+        SubtractionExpression subtractionExpression = (SubtractionExpression) expression;
+        NumberExpression leftNumber = (NumberExpression) subtractionExpression.getMinuend();
         assertEquals(42, leftNumber.evaluate());
-        NumberExpression rightNumber = (NumberExpression) addExpression.getSubtrahend();
+        NumberExpression rightNumber = (NumberExpression) subtractionExpression.getSubtrahend();
         assertEquals(28, rightNumber.evaluate());
         assertEquals(14, expression.evaluate());
     }
@@ -115,10 +115,10 @@ public class ExpressionCreatorTest {
         ExpressionCreator ec = new ExpressionCreator(tokenizer);
         IExpression expression = ec.createExpression();
         assertTrue("Expression is NOT a Multiplication", expression instanceof MultiplicationExpression);
-        MultiplicationExpression addExpression = (MultiplicationExpression) expression;
-        NumberExpression multiplier = (NumberExpression) addExpression.getMultiplier();
+        MultiplicationExpression multiplicationExpression = (MultiplicationExpression) expression;
+        NumberExpression multiplier = (NumberExpression) multiplicationExpression.getMultiplier();
         assertEquals(6, multiplier.evaluate());
-        NumberExpression multicand = (NumberExpression) addExpression.getMulticand();
+        NumberExpression multicand = (NumberExpression) multiplicationExpression.getMulticand();
         assertEquals(7, multicand.evaluate());
         assertEquals(42, expression.evaluate());
     }
@@ -133,11 +133,183 @@ public class ExpressionCreatorTest {
         ExpressionCreator ec = new ExpressionCreator(tokenizer);
         IExpression expression = ec.createExpression();
         assertTrue("Expression is NOT a Division", expression instanceof DivisionExpression);
-        DivisionExpression addExpression = (DivisionExpression) expression;
-        NumberExpression dividend = (NumberExpression) addExpression.getDividend();
+        DivisionExpression divisionExpression = (DivisionExpression) expression;
+        NumberExpression dividend = (NumberExpression) divisionExpression.getDividend();
         assertEquals(42, dividend.evaluate());
-        NumberExpression divisor = (NumberExpression) addExpression.getDivisor();
+        NumberExpression divisor = (NumberExpression) divisionExpression.getDivisor();
         assertEquals(7, divisor.evaluate());
         assertEquals(6, expression.evaluate());
+    }
+
+    @Test
+    public void whenMoreNumbersAreGivenWithAdditionOperatorsThatShouldBeResultedAsAHierarchy() {
+        List<IToken> testTokens = new LinkedList<>();
+        testTokens.add((INumber) () -> 1);
+        testTokens.add(new IAddition() {});
+        testTokens.add((INumber) () -> 2);
+        testTokens.add(new IAddition() {});
+        testTokens.add((INumber) () -> 3);
+        testTokens.add(new IAddition() {});
+        testTokens.add((INumber) () -> 4);
+        ITokenizer tokenizer = new TokenizerStub(testTokens);
+        ExpressionCreator ec = new ExpressionCreator(tokenizer);
+
+        IExpression expression = ec.createExpression();
+        assertTrue("Expression is NOT an Addition", expression instanceof AdditionExpression);
+
+        AdditionExpression addExpression1 = (AdditionExpression) expression;
+        IExpression augend1 = addExpression1.getAugend();
+        IExpression addend1 = addExpression1.getAddend();
+        assertTrue("First augend is NOT an Addition", augend1 instanceof AdditionExpression);
+        assertTrue("First addend is NOT a Number", addend1 instanceof NumberExpression);
+        assertEquals(6, augend1.evaluate());
+        assertEquals(4, addend1.evaluate());
+
+        AdditionExpression addExpression2 = (AdditionExpression) augend1;
+        IExpression augend2 = addExpression2.getAugend();
+        IExpression addend2 = addExpression2.getAddend();
+        assertTrue("Second augend is NOT an Addition", augend2 instanceof AdditionExpression);
+        assertTrue("Second addend is NOT a Number", addend2 instanceof NumberExpression);
+        assertEquals(3, augend2.evaluate());
+        assertEquals(3, addend2.evaluate());
+
+        AdditionExpression addExpression3 = (AdditionExpression) augend2;
+        IExpression augend3 = addExpression3.getAugend();
+        IExpression addend3 = addExpression3.getAddend();
+        assertTrue("Third augend is NOT a Number", augend3 instanceof NumberExpression);
+        assertTrue("Third addend is NOT a Number", addend3 instanceof NumberExpression);
+        assertEquals(1, augend3.evaluate());
+        assertEquals(2, addend3.evaluate());
+
+        assertEquals(10, expression.evaluate());
+    }
+
+    @Test
+    public void whenMoreNumbersAreGivenWithSubtractionOperatorsThatShouldBeResultedAsAHierarchy() {
+        List<IToken> testTokens = new LinkedList<>();
+        testTokens.add((INumber) () -> 100);
+        testTokens.add(new IMinus() {});
+        testTokens.add((INumber) () -> 50);
+        testTokens.add(new IMinus() {});
+        testTokens.add((INumber) () -> 30);
+        testTokens.add(new IMinus() {});
+        testTokens.add((INumber) () -> 10);
+        ITokenizer tokenizer = new TokenizerStub(testTokens);
+        ExpressionCreator ec = new ExpressionCreator(tokenizer);
+
+        IExpression expression = ec.createExpression();
+        assertTrue("Expression is NOT an Subtraction", expression instanceof SubtractionExpression);
+
+        SubtractionExpression subtractionExpression1 = (SubtractionExpression) expression;
+        IExpression minuend1 = subtractionExpression1.getMinuend();
+        IExpression subtrahend1 = subtractionExpression1.getSubtrahend();
+        assertTrue("First augend is NOT an Subtraction", minuend1 instanceof SubtractionExpression);
+        assertTrue("First addend is NOT a Number", subtrahend1 instanceof NumberExpression);
+        assertEquals(20, minuend1.evaluate());
+        assertEquals(10, subtrahend1.evaluate());
+
+        SubtractionExpression subtractionExpression2 = (SubtractionExpression) minuend1;
+        IExpression minuend2 = subtractionExpression2.getMinuend();
+        IExpression subtrahend2 = subtractionExpression2.getSubtrahend();
+        assertTrue("Second augend is NOT an Subtraction", minuend2 instanceof SubtractionExpression);
+        assertTrue("Second addend is NOT a Number", subtrahend2 instanceof NumberExpression);
+        assertEquals(50, minuend2.evaluate());
+        assertEquals(30, subtrahend2.evaluate());
+
+        SubtractionExpression subtractionExpression3 = (SubtractionExpression) minuend2;
+        IExpression minuend3 = subtractionExpression3.getMinuend();
+        IExpression subtrahend3 = subtractionExpression3.getSubtrahend();
+        assertTrue("Third augend is NOT a Number", minuend3 instanceof NumberExpression);
+        assertTrue("Third addend is NOT a Number", subtrahend3 instanceof NumberExpression);
+        assertEquals(100, minuend3.evaluate());
+        assertEquals(50, subtrahend3.evaluate());
+
+        assertEquals(10, expression.evaluate());
+    }
+
+    @Test
+    public void whenMoreNumbersAreGivenWithMultiplicationOperatorsThatShouldBeResultedAsAHierarchy() {
+        List<IToken> testTokens = new LinkedList<>();
+        testTokens.add((INumber) () -> 1);
+        testTokens.add(new IMultiplication() {});
+        testTokens.add((INumber) () -> 2);
+        testTokens.add(new IMultiplication() {});
+        testTokens.add((INumber) () -> 3);
+        testTokens.add(new IMultiplication() {});
+        testTokens.add((INumber) () -> 4);
+        ITokenizer tokenizer = new TokenizerStub(testTokens);
+        ExpressionCreator ec = new ExpressionCreator(tokenizer);
+
+        IExpression expression = ec.createExpression();
+        assertTrue("Expression is NOT an Multiplication", expression instanceof MultiplicationExpression);
+
+        MultiplicationExpression multiplicationExpression1 = (MultiplicationExpression) expression;
+        IExpression multiplier1 = multiplicationExpression1.getMultiplier();
+        IExpression multicand1 = multiplicationExpression1.getMulticand();
+        assertTrue("First augend is NOT an Multiplication", multiplier1 instanceof MultiplicationExpression);
+        assertTrue("First addend is NOT a Number", multicand1 instanceof NumberExpression);
+        assertEquals(6, multiplier1.evaluate());
+        assertEquals(4, multicand1.evaluate());
+
+        MultiplicationExpression multiplicationExpression2 = (MultiplicationExpression) multiplier1;
+        IExpression multiplier2 = multiplicationExpression2.getMultiplier();
+        IExpression multicand2 = multiplicationExpression2.getMulticand();
+        assertTrue("Second augend is NOT an Multiplication", multiplier2 instanceof MultiplicationExpression);
+        assertTrue("Second addend is NOT a Number", multicand2 instanceof NumberExpression);
+        assertEquals(2, multiplier2.evaluate());
+        assertEquals(3, multicand2.evaluate());
+
+        MultiplicationExpression multiplicationExpression3 = (MultiplicationExpression) multiplier2;
+        IExpression multiplier3 = multiplicationExpression3.getMultiplier();
+        IExpression multicand3 = multiplicationExpression3.getMulticand();
+        assertTrue("Third augend is NOT a Number", multiplier3 instanceof NumberExpression);
+        assertTrue("Third addend is NOT a Number", multicand3 instanceof NumberExpression);
+        assertEquals(1, multiplier3.evaluate());
+        assertEquals(2, multicand3.evaluate());
+
+        assertEquals(24, expression.evaluate());
+    }
+
+    @Test
+    public void whenMoreNumbersAreGivenWithDivisionOperatorsThatShouldBeResultedAsAHierarchy() {
+        List<IToken> testTokens = new LinkedList<>();
+        testTokens.add((INumber) () -> 72);
+        testTokens.add(new IDivision() {});
+        testTokens.add((INumber) () -> 4);
+        testTokens.add(new IDivision() {});
+        testTokens.add((INumber) () -> 3);
+        testTokens.add(new IDivision() {});
+        testTokens.add((INumber) () -> 2);
+        ITokenizer tokenizer = new TokenizerStub(testTokens);
+        ExpressionCreator ec = new ExpressionCreator(tokenizer);
+
+        IExpression expression = ec.createExpression();
+        assertTrue("Expression is NOT an Division", expression instanceof DivisionExpression);
+
+        DivisionExpression divisionExpression1 = (DivisionExpression) expression;
+        IExpression dividend1 = divisionExpression1.getDividend();
+        IExpression divisor1 = divisionExpression1.getDivisor();
+        assertTrue("First dividend is NOT an Division", dividend1 instanceof DivisionExpression);
+        assertTrue("First divisor is NOT a Number", divisor1 instanceof NumberExpression);
+        assertEquals(6, dividend1.evaluate());
+        assertEquals(2, divisor1.evaluate());
+
+        DivisionExpression subtractionExpression2 = (DivisionExpression) dividend1;
+        IExpression dividend2 = subtractionExpression2.getDividend();
+        IExpression divisor2 = subtractionExpression2.getDivisor();
+        assertTrue("Second dividend is NOT an Division", dividend2 instanceof DivisionExpression);
+        assertTrue("Second divisor is NOT a Number", divisor2 instanceof NumberExpression);
+        assertEquals(18, dividend2.evaluate());
+        assertEquals(3, divisor2.evaluate());
+
+        DivisionExpression subtractionExpression3 = (DivisionExpression) dividend2;
+        IExpression dividend3 = subtractionExpression3.getDividend();
+        IExpression divisor3 = subtractionExpression3.getDivisor();
+        assertTrue("Third dividend is NOT a Number", dividend3 instanceof NumberExpression);
+        assertTrue("Third divisor is NOT a Number", divisor3 instanceof NumberExpression);
+        assertEquals(72, dividend3.evaluate());
+        assertEquals(4, divisor3.evaluate());
+
+        assertEquals(3, expression.evaluate());
     }
 }
